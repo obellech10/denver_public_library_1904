d7775d02fdbb18e7283dfb595d1a11cea9c79b73
Oscar Bellechasse <obellech10@gmail.com>
2019-05-22 15:33:32 -0600

    Initial commit

---
+++ test/library_test.rb
@@ -0,0 +1,19 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/book'
+require './lib/author'
+require './lib/library'
+
+
+class LibraryTest < MiniTest::Test
+
+  def setup
+    @dpl = Library.new("Denver Public Library")
+  end
+
+  def test_that_it_exits
+    assert_instance_of Library, @dpl
+  end
+end
