fd6416e6adb845bf9f512f56d837b00234d3649f
Oscar Bellechasse <obellech10@gmail.com>
2019-05-22 15:37:49 -0600

    Add tests for attributes

--- test/library_test.rb
+++ test/library_test.rb
@@ -16,4 +16,10 @@ class LibraryTest < MiniTest::Test
   def test_that_it_exits
     assert_instance_of Library, @dpl
   end
+
+  def test_its_attributes
+    assert_equal "Denver Public Library", @dpl.name
+    assert_equal [], @dpl.books
+    assert_equal [], @dpl.authors 
+  end
 end
