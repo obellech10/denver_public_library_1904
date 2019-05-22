b383957795fa4e596f43d04fdb81608086ac5c57
Oscar Bellechasse <obellech10@gmail.com>
2019-05-22 15:29:51 -0600

    Create tests for write and add_book method

--- test/author_test.rb
+++ test/author_test.rb
@@ -21,11 +21,21 @@ class AuthorTest < MiniTest::Test
   end
 
   def test_that_author_can_write
-    book_1 = Book.new({author_first_name: "Charlotte", author_last_name: "Bronte", title: "Jane Erye", publication_date: "October 16, 1847"})
+    # book_1 = Book.new({author_first_name: "Charlotte", author_last_name: "Bronte", title: "Jane Erye", publication_date: "October 16, 1847"})
     jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    @charlotte_bronte.add_book
 
+    assert_equal [jane_eyre], @charlotte_bronte.books
     assert_equal Book, jane_eyre.class
     assert_equal "Jane Eyre", jane_eyre.title
   end
 
+  def test_that_author_now_has_books
+    book_1 = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    @charlotte_bronte.add_book
+    book_2 = @charlotte_bronte.write("Villette", "1853")
+    @charlotte_bronte.add_book
+
+    assert_equal [book_1, book_2], @charlotte_bronte.books
+  end
 end
