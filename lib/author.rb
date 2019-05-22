86dd12e2fbb70bc79c222b67b3b101844bf57d25
Oscar Bellechasse <obellech10@gmail.com>
2019-05-22 15:29:25 -0600

    Add write and add_book method

--- lib/author.rb
+++ lib/author.rb
@@ -1,15 +1,21 @@
 class Author
-  attr_reader :name,
-              :books
+  attr_reader :name
+
+  attr_accessor :books
 
   def initialize(author_info)
     @first_name = author_info[:first_name]
     @last_name = author_info[:last_name]
     @name = "#{@first_name} #{@last_name}"
     @books = []
+    @book_to_add = []
   end
 
   def write(title, publication_date)
-    Book.new(author_first_name: @first_name, author_last_name: @last_name, title: title, publication_date: publication_date)
+    @book_to_add = Book.new(author_first_name: @first_name, author_last_name: @last_name, title: title, publication_date: publication_date)
+  end
+
+  def add_book
+    @books << @book_to_add
   end
 end
