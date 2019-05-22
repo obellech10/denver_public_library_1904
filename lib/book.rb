2f022e6bbe3436989de73a1f0742e1670315e636
Oscar Bellechasse <obellech10@gmail.com>
2019-05-22 14:28:42 -0600

    Create attr readers for attributes

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,14 @@
 class Book
+  attr_reader :title,
+              :author,
+              :publication_year
 
   def initialize(book_info)
-
+    @author_first_name = book_info[:author_first_name]
+    @author_last_name = book_info[:author_last_name]
+    @author = "#{@author_first_name} #{@author_last_name}"
+    @title = book_info[:title]
+    @publication_date = book_info[:publication_date]
+    @publication_year = @publication_date.slice(-4..-1)
   end
 end
